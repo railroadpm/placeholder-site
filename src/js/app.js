286fb2cb7d78c05aa189c5662ccd52c0e8493b14
SLathrop <steve@stela.io>
2018-04-04 16:16:50 -0400

    Clean dist

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,9 @@
 // JS Goes here - ES6 supported
 if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", (user) => {
+  window.netlifyIdentity.on('init', (user) => {
     if (!user) {
-      window.netlifyIdentity.on("login", () => {
-        document.location.href = "/extranet/";
+      window.netlifyIdentity.on('login', () => {
+        document.location.href = '/extranet';
       });
     }
   });
