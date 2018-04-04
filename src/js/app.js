43b452d06548fc73466bf3bfb9b67c792e1b6abc
SLathrop <steve@stela.io>
2018-04-04 16:43:09 -0400

    Need fix for naked folder/redirect

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ if (window.netlifyIdentity) {
   window.netlifyIdentity.on('init', (user) => {
     if (!user) {
       window.netlifyIdentity.on('login', () => {
-        document.location.href = '/extranet';
+        document.location.href = '/extranet/';
       });
     }
   });
